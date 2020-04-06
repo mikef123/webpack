0833b9b6e50df5c854ea91d825b7f749cdaedf49
miguel <miguelferu@gmail.com>
2020-04-06 12:22:13 -0500

    scripts types

--- external/webpack.config.js
+++ external/webpack.config.js
@@ -2,9 +2,8 @@ const path = require('path')
 
 module.exports = {
     entry: path.resolve(__dirname,'index.js'),
-    mode: 'development',
     output: {
-        path: path.resolve(__dirname),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     }
 }
